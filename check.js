200f3c5ef22c9d1dd88c0b3eee95e72e77f24b10
RYESAN58 <92766524+RYESAN58@users.noreply.github.com>
2022-03-08 23:13:17 -0500

    some es6 practice!

---
+++ check.js
@@ -0,0 +1,51 @@
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+//error will occur here because bracks are supposed to be curly
+
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+//output will be name will throw error and other name will be Elon
+
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+//output will be the string 12345 and the second output will be an error
+
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+
+// will out put False and then True
+
+
+
+
